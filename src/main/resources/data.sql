4c18277c962fcbbf72a432682fe6114bb169bc08
VeerMuchandi <veer.muchandi@gmail.com>
2016-11-15 15:11:12 -0500

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-delete * from customer; 
+delete from customer; 
 insert into customer values (null, "Joe", 88);
 insert into customer values (null, "Jack", 54);
 insert into customer values (null, "Ann", 32);
