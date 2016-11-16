a83db6e6a802d30bde5eea6f301b1adf3118860e
VeerMuchandi <veer.muchandi@gmail.com>
2016-11-16 18:54:17 -0500

    separated scripts by database type

---
+++ 2tier/src/main/resources/data-hsqldb.sql
@@ -0,0 +1,5 @@
+delete from customer; 
+insert into customer (name, age) values (null, 'Joe', 88);
+insert into customer (name, age) values (null, 'Jack', 54);
+insert into customer (name, age) values (null, 'Ann', 32);
+
