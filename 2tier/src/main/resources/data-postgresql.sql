87c57f429217069172dbc682ddeb4474d0879b65
VeerMuchandi <veer.muchandi@gmail.com>
2016-11-16 23:14:25 -0500

    added postgresql scripts

---
+++ 2tier/src/main/resources/data-postgresql.sql
@@ -0,0 +1,5 @@
+delete from customer; 
+insert into customer (name,age) values ('Joe', 88);
+insert into customer (name,age) values ('Jack', 54);
+insert into customer (name,age) values ('Ann', 32);
+
